058c4e5bca1ef102f994a0aa8e482da0db9d495c
janrebolledo <janrebolledo63@gmail.com>
2022-01-29 22:16:47 -0800

    Added logic for new entry

--- src/pages/log.js
+++ src/pages/log.js
@@ -2,17 +2,44 @@ import "../styles/log.css";
 
 export default function Log() {
   function newEntry() {
-    var date = document.getElementById("date").value;
+    var date = document.getElementById("date").value.replace(/-/g, "/");
     var name = document.getElementById("name").value;
-    var amount = document.getElementById("amount").value;
+    var amount = document
+      .getElementById("amount")
+      .value.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    // Get log in localStorage
+    let log = JSON.parse(localStorage.getItem("log") || "[]");
+    // Set newEntry as a blank array
     let newEntry = [];
 
+    // Check if all inputs are filled
     if ((date !== "", name !== "", amount !== "")) {
+      // Set id as random number
       let id = Math.floor(Math.random() * 100000);
-      newEntry.push(id, date, name, amount);
-      localStorage.setItem("log", JSON.stringify(newEntry));
+      // Create new entry
+      newEntry.push({ id, date, name, amount });
+      // Add newEntry to current log
+      let updatedLog = log.concat(newEntry);
+      // Set updatedLog as localStorage
+      localStorage.setItem("log", JSON.stringify(updatedLog));
     }
   }
+  function deleteItem(event) {
+    // Get id of item by getting id of button
+    const removeId = event.target.id;
+    console.log(removeId);
+
+    // Get log from localStorage
+    // let log = JSON.parse(localStorage.getItem("log") || "[]");
+
+    // To-do: remove item from localstorage
+  }
+
+  // TO-DO
+  // Refresh map on new entry
   let log = JSON.parse(localStorage.getItem("log") || "[]");
   return (
     <main>
@@ -25,18 +52,19 @@ export default function Log() {
         </div>
         <div className="log" id="log">
           {log.map((item) => (
-            <div className="log-item" key={item}>
+            <div className="log-item" key={item.id} id={item.id}>
               <p>{item.date}</p>
               <p>{item.name}</p>
-              <p>{item.amount}</p>
+              <p>${item.amount}</p>
+              <span
+                className="material-icons"
+                id={item.id}
+                onClick={deleteItem}
+              >
+                close
+              </span>
             </div>
           ))}
-          <div className="log-item">
-            <p>1/1/2022</p>
-            <p>Expense</p>
-            <p>$10.00</p>
-            <span className="material-icons">close</span>
-          </div>
         </div>
         <p className="log-total">
           Total: <b>$10.00</b>
