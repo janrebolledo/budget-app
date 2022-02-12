60cf2b7565e8fcaefa43505fb37112a8ec547582
janrebolledo <janrebolledo63@gmail.com>
2022-02-12 11:29:16 -0800

    Monthly Insight Finished + Initial Yearly Insight

--- src/pages/home.js
+++ src/pages/home.js
@@ -104,8 +104,208 @@ export default function Home() {
     }
   }
 
+  function monthlyInsight() {
+    const log = JSON.parse(localStorage.getItem("log") || "[]");
+
+    const monthlyTotalH3 = document.getElementById("monthly-total");
+
+    // Gets number of week in year
+    var monthNum = LocalDate.now().monthValue();
+    var yearNum = LocalDate.now().year();
+
+    // Filters the list by if week number is the same
+    var monthlyLog = log.filter((item) => {
+      return item.month === monthNum;
+    });
+
+    // Makes sure that the year is the same
+    monthlyLog = monthlyLog.filter((item) => {
+      return item.year === yearNum;
+    });
+
+    // Get amounts in an array as integers
+    let result = monthlyLog.map(({ amount }) =>
+      Number(amount.replace("$", "").replace(/,/g, ""))
+    );
+
+    // Add amounts together
+    let total = 0;
+
+    for (let i = 0; i < result.length; i++) {
+      total += result[i];
+    }
+
+    const formattedTotal = total.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    const prevMonthTotal = document.getElementById("prev-month-total");
+
+    // Gets number of previous month in year
+
+    if (monthNum === 1) {
+      var prevMonthNum = 12;
+      var prevYearNum = yearNum - 1;
+    } else {
+      // eslint-disable-next-line
+      var prevMonthNum = LocalDate.now().monthValue() - 1;
+      // eslint-disable-next-line
+      var prevYearNum = LocalDate.now().year();
+    }
+
+    // Filters the list by if month number is the same
+
+    var prevMonthlyLog = log.filter((item) => {
+      return item.month === prevMonthNum;
+    });
+
+    // Makes sure that the year is the same
+    prevMonthlyLog = prevMonthlyLog.filter((item) => {
+      return item.year === prevYearNum;
+    });
+
+    // Get amounts in an array as integers
+    let prevResult = prevMonthlyLog.map(({ amount }) =>
+      Number(amount.replace("$", "").replace(/,/g, ""))
+    );
+
+    // Add amounts together
+    let prevTotal = 0;
+
+    for (let i = 0; i < prevResult.length; i++) {
+      prevTotal += prevResult[i];
+    }
+
+    const prevFormattedTotal = prevTotal.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    const percentage = Number(((total / prevTotal) * 100).toFixed(2));
+
+    const monthlyPercentageSpan = document.getElementById(
+      "monthly-insight-percentage"
+    );
+    const monthlyPercentageIndicatorSpan = document.getElementById(
+      "monthly-percentage-indicator"
+    );
+
+    if (total > 0) {
+      monthlyTotalH3.innerHTML = formattedTotal;
+      prevMonthTotal.innerHTML = prevFormattedTotal;
+      monthlyPercentageSpan.innerHTML = percentage;
+      if (percentage > 100) {
+        monthlyPercentageIndicatorSpan.innerHTML = "arrow_drop_up";
+      } else {
+        monthlyPercentageIndicatorSpan.innerHTML = "arrow_drop_down";
+      }
+    }
+  }
+
+  function yearlyInsight() {
+    const log = JSON.parse(localStorage.getItem("log") || "[]");
+
+    const weeklyTotalH3 = document.getElementById("yearly-total");
+
+    // Gets number of week in year
+    var weekNum = LocalDate.now().isoWeekOfWeekyear();
+    var yearNum = LocalDate.now().year();
+
+    // Filters the list by if week number is the same
+    var weeklyLog = log.filter((item) => {
+      return item.week === weekNum;
+    });
+
+    // Makes sure that the year is the same
+    weeklyLog = weeklyLog.filter((item) => {
+      return item.year === yearNum;
+    });
+
+    // Get amounts in an array as integers
+    let result = weeklyLog.map(({ amount }) =>
+      Number(amount.replace("$", "").replace(/,/g, ""))
+    );
+
+    // Add amounts together
+    let total = 0;
+
+    for (let i = 0; i < result.length; i++) {
+      total += result[i];
+    }
+
+    const formattedTotal = total.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    const prevWeekTotal = document.getElementById("prev-year-total");
+
+    // Gets number of previous week in year
+
+    if (weekNum === 1) {
+      var prevWeekNum = 52;
+      var prevYearNum = yearNum - 1;
+    } else {
+      // eslint-disable-next-line
+      var prevWeekNum = LocalDate.now().isoWeekOfWeekyear() - 1;
+      // eslint-disable-next-line
+      var prevYearNum = LocalDate.now().year();
+    }
+
+    // Filters the list by if week number is the same
+
+    var prevWeeklyLog = log.filter((item) => {
+      return item.week === prevWeekNum;
+    });
+
+    // Makes sure that the year is the same
+    prevWeeklyLog = prevWeeklyLog.filter((item) => {
+      return item.year === prevYearNum;
+    });
+
+    // Get amounts in an array as integers
+    let prevResult = prevWeeklyLog.map(({ amount }) =>
+      Number(amount.replace("$", "").replace(/,/g, ""))
+    );
+
+    // Add amounts together
+    let prevTotal = 0;
+
+    for (let i = 0; i < prevResult.length; i++) {
+      prevTotal += prevResult[i];
+    }
+
+    const prevFormattedTotal = prevTotal.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    const percentage = Number(((total / prevTotal) * 100).toFixed(2));
+
+    const weeklyPercentageSpan = document.getElementById(
+      "yearly-insight-percentage"
+    );
+    const weeklyPercentageIndicatorSpan = document.getElementById(
+      "yearly-percentage-indicator"
+    );
+
+    if (total > 0) {
+      weeklyTotalH3.innerHTML = formattedTotal;
+      prevWeekTotal.innerHTML = prevFormattedTotal;
+      weeklyPercentageSpan.innerHTML = percentage;
+      if (percentage > 100) {
+        weeklyPercentageIndicatorSpan.innerHTML = "arrow_drop_up";
+      } else {
+        weeklyPercentageIndicatorSpan.innerHTML = "arrow_drop_down";
+      }
+    }
+  }
+
   useEffect(() => {
     weeklyInsight();
+    monthlyInsight();
+    yearlyInsight();
   });
 
   let log = JSON.parse(localStorage.getItem("log"));
@@ -207,10 +407,18 @@ export default function Home() {
           <div className="card">
             <h4>Monthly Spending</h4>
             <div>
-              <h3 className="card-total">—</h3>
+              <h3 className="card-total" id="monthly-total">
+                —
+              </h3>
               <small>
-                <span className="material-icons">arrow_drop_up</span>
-                0% (Previously: <span id="prev-month-total">—</span> )
+                <span
+                  className="material-icons"
+                  id="monthly-percentage-indicator"
+                >
+                  arrow_drop_up
+                </span>
+                <span id="monthly-insight-percentage">0</span>% (Previously:{" "}
+                <span id="prev-month-total">—</span> )
               </small>
             </div>
             {/* <Chart type="month" /> */}
@@ -221,10 +429,18 @@ export default function Home() {
           <div className="card">
             <h4>Annual Spending</h4>
             <div>
-              <h3 className="card-total">—</h3>
+              <h3 className="card-total" id="yearly-total">
+                —
+              </h3>
               <small>
-                <span className="material-icons">arrow_drop_up</span>
-                0% (Previously: <span id="prev-year-total">—</span> )
+                <span
+                  className="material-icons"
+                  id="yearly-percentage-indicator"
+                >
+                  arrow_drop_up
+                </span>
+                <span id="yearly-insight-percentage">0</span>% (Previously:{" "}
+                <span id="prev-year-total">—</span> )
               </small>
             </div>
             {/* <Chart type="year" /> */}
